8bc4ec6f6354c76b27145b4f72b8eea991e1139d
masterLuo <1335718083@qq.com>
2022-03-05 00:05:57 +0800

    默认变更列表

--- servicemom/service_oss/src/main/java/com/kun/oss/service/impl/OssServiceImpl.java
+++ servicemom/service_oss/src/main/java/com/kun/oss/service/impl/OssServiceImpl.java
@@ -20,11 +20,11 @@ public class OssServiceImpl implements OssService {
     @Override
     public String uploadFileAvatar(MultipartFile file) {
         // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
-        String endpoint = ConstantPropertiesUtils.ENDPOINT;
+        String endpoint = "111";
 // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = ConstantPropertiesUtils.KEYID;
-        String accessKeySecret = ConstantPropertiesUtils.KEYSECRET;
-        String originalFilename = file.getOriginalFilename();
+        String accessKeyId = "111";
+        String accessKeySecret = "111";
+        String originalFilename = "111";
 
         String datePath = new DateTime().toString("yyyy-MM-dd");
         String uuid = UUID.randomUUID().toString().replaceAll("-","");
